c3bbfe4073dc718f157664bc09961adc540c80fb
dscao <dscao.com@gmail.com>
2022-10-16 11:12:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,6 +19,9 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 
 svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata package/luci-app-netdata
 
+
+git clone -b master https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+
 # 流量监控
 # svn co https://github.com/haiibo/packages/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
 # svn co https://github.com/haiibo/packages/trunk/wrtbwmon package/wrtbwmon
