64653dba0b68609a058ee453c62b65c588c96c9d
dscao <dscao.com@gmail.com>
2023-10-30 19:37:29 +0800

    Update ds-diy-part2.sh

--- ds-diy-part2.sh
+++ ds-diy-part2.sh
@@ -21,11 +21,11 @@ svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-netdata packa
 
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
-# svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+# svn co https://github.com/dscao/small-package/trunk/luci-app-adguardhome package/luci-app-adguardhome
 
 # git clone https://github.com/AdguardTeam/AdGuardHome.git package/adguardhome
 
-git clone https://github.com/davintagas/luci-app-adguardhome.git package/luci-app-adguardhome
+# git clone https://github.com/davintagas/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 
